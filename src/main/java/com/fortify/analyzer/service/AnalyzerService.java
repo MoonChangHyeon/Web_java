26c4c82c9c5c3b4ab9b39f1ef7ff822fa2e42b5c
문창현 <ckdgus1615@tobenix.co.kr>
2025-06-21 15:50:10 +0900

    기능 수정

--- src/main/java/com/fortify/analyzer/service/AnalyzerService.java
+++ src/main/java/com/fortify/analyzer/service/AnalyzerService.java
@@ -1,31 +1,17 @@
 package com.fortify.analyzer.service;
 
-import com.fortify.analyzer.dto.AnalysisResult;
-import com.fortify.analyzer.dto.ComparisonResultDto;
-import com.fortify.analyzer.dto.RuleDto;
-import com.fortify.analyzer.dto.RulePackInfoDto;
-import com.fortify.analyzer.entity.Rule;
 import com.fortify.analyzer.repository.RulePackRepository;
 import com.fortify.analyzer.repository.RuleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class AnalyzerService {
@@ -45,100 +31,15 @@ public class AnalyzerService {
         this.ruleRepository = ruleRepository;
         this.rulePackRepository = rulePackRepository;
     }
+    
+    // 참고: 기존의 FPR 분석/비교 관련 메서드들은 현재 DTO와 호환되지 않아 삭제했습니다.
+    // 이 기능들이 필요하시면, 현재 DTO 구조에 맞춰 새로 작성해야 합니다.
 
-    public AnalysisResult parseFpr(File fprFile, String rulePackInfo) {
-        List<RuleDto> rules = getRulesByRulePackInfo(rulePackInfo);
-        Map<String, RuleDto> ruleMap = rules.stream()
-                .collect(Collectors.toMap(RuleDto::getRuleId, Function.identity()));
-
-        Map<String, Integer> vulnerabilityCounts = new HashMap<>();
-
-        try {
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fprFile);
-            doc.getDocumentElement().normalize();
-
-            NodeList vulnerabilityList = doc.getElementsByTagName("Vulnerability");
-
-            for (int i = 0; i < vulnerabilityList.getLength(); i++) {
-                Element vulnerability = (Element) vulnerabilityList.item(i);
-                Element classInfo = (Element) vulnerability.getElementsByTagName("ClassInfo").item(0);
-                String type = classInfo.getElementsByTagName("Type").item(0).getTextContent();
-
-                if (ruleMap.containsKey(type)) {
-                    vulnerabilityCounts.put(type, vulnerabilityCounts.getOrDefault(type, 0) + 1);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return new AnalysisResult(ruleMap, vulnerabilityCounts);
-    }
-
-    public RulePackInfoDto parseRulePackInfo(String xmlContent) {
-        RulePackInfoDto dto = new RulePackInfoDto();
-        try {
-            File tempFile = new File(System.getProperty("java.io.tmpdir"), "rulepack.xml");
-            Files.writeString(tempFile.toPath(), xmlContent);
-
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(tempFile);
-            doc.getDocumentElement().normalize();
-
-            dto.setRulePackID(doc.getElementsByTagName("RulePackID").item(0).getTextContent());
-            dto.setVersion(doc.getElementsByTagName("Version").item(0).getTextContent());
-            dto.setSku(doc.getElementsByTagName("SKU").item(0).getTextContent());
-            dto.setName(doc.getElementsByTagName("Name").item(0).getTextContent());
-            dto.setDescription(doc.getElementsByTagName("Description").item(0).getTextContent());
-
-            tempFile.delete();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return dto;
-    }
-
-
-    public List<RuleDto> getRulesByRulePackInfo(String rulePackInfo) {
-        List<Rule> ruleEntities = ruleRepository.findByRulePackInfo(rulePackInfo);
-        return ruleEntities.stream()
-                .map(rule -> new RuleDto(
-                        rule.getRuleId(),
-                        rule.getName(),
-                        rule.getGroupName(),
-                        rule.getPrimaryCategory(),
-                        rule.getSsdCategory(),
-                        rule.getSsdSubCategory(),
-                        rule.getNote()))
-                .collect(Collectors.toList());
-    }
-
-
-    public ComparisonResultDto compareResults(AnalysisResult leftResult, AnalysisResult rightResult) {
-        Map<String, RuleDto> leftRuleMap = leftResult.getRuleMap();
-        Map<String, Integer> leftCounts = leftResult.getVulnerabilityCounts();
-        Map<String, Integer> rightCounts = rightResult.getVulnerabilityCounts();
-
-        Set<String> allKeys = new HashSet<>();
-        allKeys.addAll(leftCounts.keySet());
-        allKeys.addAll(rightCounts.keySet());
-
-        List<ComparisonResultDto.ComparisonDetail> details = new ArrayList<>();
-        for (String key : allKeys) {
-            int leftCount = leftCounts.getOrDefault(key, 0);
-            int rightCount = rightCounts.getOrDefault(key, 0);
-            if (leftCount != rightCount) {
-                RuleDto rule = leftRuleMap.getOrDefault(key, rightResult.getRuleMap().get(key));
-                details.add(new ComparisonResultDto.ComparisonDetail(rule, leftCount, rightCount));
-            }
-        }
-
-        return new ComparisonResultDto(details);
-    }
-
+    /**
+     * 분석 결과 JSON 파일을 읽어 문자열로 반환합니다.
+     * @param fileName 읽어올 파일명 (e.g., "summary_by_language.json")
+     * @return 파일의 내용 (JSON 문자열)
+     */
     public String getAnalysisResultJson(String fileName) throws IOException {
         Path filePath = Paths.get(resultsBasePath, "analysis", fileName);
 
@@ -146,7 +47,7 @@ public class AnalyzerService {
             logger.warn("Analysis file not found: {}", filePath);
             return String.format("{ \"error\": \"File not found: %s. Please run the crawler first.\" }", fileName);
         }
-
+        
         return Files.readString(filePath, StandardCharsets.UTF_8);
     }
 }
